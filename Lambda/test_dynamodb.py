de7e6103c7cbbed423fa85f7a4811991140fbb84
rayssd <rayssd@hotmail.com>
2020-12-14 23:33:19 +1100

    Added Python workflow

--- Lambda/test_dynamodb.py
+++ Lambda/test_dynamodb.py
@@ -2,16 +2,14 @@ import dynamodb
 import boto3
 
 
-ddb = boto3.resource('dynamodb')
-MyTable = ddb.Table('MyTable')
-
-
-def test_updateDDBItem(MyTable):
+def test_updateDDBItem():
+    ddb = boto3.resource('dynamodb')
+    MyTable = ddb.Table('MyTable')
     original = dynamodb.readDDBItem(MyTable)
     target = original + 1
     assert dynamodb.updateDDBItem(MyTable) == target, f"Should be {original}!"
 
 
 if __name__ == "__main__":
-    test_updateDDBItem(MyTable)
-    print("Everything passed")
+    test_updateDDBItem()
+    # print("Everything passed")
